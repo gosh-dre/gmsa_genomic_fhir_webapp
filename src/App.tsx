1599554af9fff7e30b8d2cffb5ee12a13741430e
James Hughes <james.hughes@ucl.ac.uk>
2022-06-17 20:57:48 +0100

    removed old fhir provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import Layout from "./components/layout/Layout";
 import { Route, Routes } from "react-router-dom";
 import NewReport from "./pages/NewReport";
 import FhirAuthoriser from "./components/fhir/FhirAuthoriser";
-import FhirProvider from "./components/fhir/FhirProvider";
 
 function App() {
   return (
     <Layout>
-      <FhirProvider>
-        <Routes>
-          <Route path="/" element={<FhirAuthoriser />} />
-          <Route path="/add_report" element={<NewReport />} />
-        </Routes>
-      </FhirProvider>
+      <Routes>
+        <Route path="/" element={<FhirAuthoriser />} />
+        <Route path="/add_report" element={<NewReport />} />
+      </Routes>
     </Layout>
   );
 }
