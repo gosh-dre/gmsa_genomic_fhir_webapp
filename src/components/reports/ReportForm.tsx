4f753416a4b383ac80ddac141f47de45a59a38d8
James Hughes <james.hughes@ucl.ac.uk>
2022-07-15 19:01:20 +0100

    removed unused import

--- src/components/reports/ReportForm.tsx
+++ src/components/reports/ReportForm.tsx
@@ -1,5 +1,5 @@
 import { FC, useContext, useRef, useState } from "react";
-import { Form, Formik, FormikHelpers, FormikProps, FieldArray } from "formik";
+import { Form, Formik, FormikHelpers, FormikProps } from "formik";
 import * as Yup from "yup";
 import { FhirContext } from "../fhir/FhirContext";
 
