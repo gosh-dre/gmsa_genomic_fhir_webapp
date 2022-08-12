b3eab0dffc3b84cad6f39fdf5e572b880681caf1
Stef Piatek <s.piatek@ucl.ac.uk>
2022-08-12 11:13:26 +0100

    Show code in drop down

--- src/components/reports/FieldSet.tsx
+++ src/components/reports/FieldSet.tsx
@@ -22,7 +22,7 @@ const FieldSet: FC<Props> = ({ name, label, selectOptions, ...rest }) => {
       <Field id={name} name={name} as="select" {...rest}>
         {selectsWithInitial.map((opt) => (
           <option key={opt.code} value={opt.code} role="select">
-            {opt.display}
+            {(opt.code && `${opt.display} (${opt.code})`) || opt.display}
           </option>
         ))}
       </Field>
