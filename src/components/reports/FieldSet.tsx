2de00297f5c43a084f7fda93f69b1809190733d0
Stefan Piatek <s.piatek@ucl.ac.uk>
2022-08-24 13:46:43 +0100

    Use span in fieldset
    Co-authored-by: James Hughes <70143648+jhughes982@users.noreply.github.com>

--- src/components/reports/FieldSet.tsx
+++ src/components/reports/FieldSet.tsx
@@ -38,11 +38,11 @@ const FieldSet: FC<Props> = ({ name, label, selectOptions, ...rest }) => {
   }
 
   return (
-    <span>
+    <>
       <label htmlFor={name}>{label}</label>
       {field}
-      <ErrorMessage name={name} component="p" className="error-text" />
-    </span>
+      <ErrorMessage name={name} component="span" className="error-text" />
+    </>
   );
 };
 
