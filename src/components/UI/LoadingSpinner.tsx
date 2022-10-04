5565311eb032af6973c235461055f28384995c6e
James Hughes <james.hughes@ucl.ac.uk>
2022-10-04 17:37:35 +0100

    applying auto formatting

--- src/components/UI/LoadingSpinner.tsx
+++ src/components/UI/LoadingSpinner.tsx
@@ -1,19 +1,19 @@
 import { FC } from "react";
 
-import classes from "./LoadingSpinner.module.css"
+import classes from "./LoadingSpinner.module.css";
 
 interface Props {
-	asOverlay: boolean;
-	message?: string;
+  asOverlay: boolean;
+  message?: string;
 }
 
 const LoadingSpinner: FC<Props> = (props) => {
-	return (
-		<div className={`${props.asOverlay && classes["loading-spinner__overlay"]}`}>
-			<div className={classes["lds-dual-ring"]}></div>
-			<div className={classes["loading-spinner__message"]}>{props.message}</div>
-		</div>
-	);
+  return (
+    <div className={`${props.asOverlay && classes["loading-spinner__overlay"]}`}>
+      <div className={classes["lds-dual-ring"]}></div>
+      <div className={classes["loading-spinner__message"]}>{props.message}</div>
+    </div>
+  );
 };
 
 export default LoadingSpinner;
