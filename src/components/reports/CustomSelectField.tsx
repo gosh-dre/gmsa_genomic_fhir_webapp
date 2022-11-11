a12eb87e68f4dc87a5785530fadac7616f5559cf
James Hughes <james.hughes@ucl.ac.uk>
2022-11-11 16:53:40 +0000

    WIP: prototype implementation of multi select working

---
+++ src/components/reports/CustomSelectField.tsx
@@ -0,0 +1,56 @@
+import { FieldProps } from "formik";
+import React from "react";
+import Select from "react-select";
+//import { OptionsType, ValueType } from "react-select/lib/types";
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+interface CustomSelectProps extends FieldProps {
+  //options: OptionsType<Option>;
+  options: any;
+  isMulti?: boolean;
+  className?: string;
+  placeholder?: string;
+}
+
+const CustomSelect = ({ className, placeholder, field, form, options, isMulti = false }: CustomSelectProps) => {
+  const onChange = (option: any) => {
+    form.setFieldValue(
+      field.name,
+      isMulti ? (option as Option[]).map((item: Option) => item.value) : (option as Option).value,
+    );
+  };
+  //   const onChange = (option: ValueType<Option | Option[]>) => {
+  //     form.setFieldValue(
+  //       field.name,
+  //       isMulti ? (option as Option[]).map((item: Option) => item.value) : (option as Option).value,
+  //     );
+  //   };
+
+  const getValue = () => {
+    if (options) {
+      return isMulti
+        ? options.filter((option: any) => field?.value?.indexOf(option.value) >= 0)
+        : options.find((option: any) => option.value === field.value);
+    } else {
+      return isMulti ? [] : ("" as any);
+    }
+  };
+
+  return (
+    <Select
+      className={className}
+      name={field.name}
+      value={getValue()}
+      onChange={onChange}
+      placeholder={placeholder}
+      options={options}
+      isMulti={isMulti}
+    />
+  );
+};
+
+export default CustomSelect;
